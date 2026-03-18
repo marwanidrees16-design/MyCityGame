583019870fb67cef50594377dab20e62e4f7661f
Marwan <marwanidress16@gmail.com>
2026-03-19 02:23:38 +0300

    تحديث القنوات

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const channels = [
         logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Aljazeera_eng.svg/1200px-Aljazeera_eng.svg.png" 
     },
     { 
-        name: "قناة رؤيا", 
+        name: "رؤيا الأردنية", 
         url: "https://roya-live.ercdn.net/roya/roya.m3u8", 
         logo: "https://roya.tv/images/roya-logo.png" 
     },
@@ -18,23 +18,33 @@ const channels = [
         name: "قناة الغد", 
         url: "https://alghad-live.ercdn.net/alghad/alghad.m3u8", 
         logo: "https://alghad.tv/wp-content/uploads/2023/logo.png" 
+    },
+    { 
+        name: "فرانس 24", 
+        url: "https://static.france24.com/live/f24_ar.m3u8", 
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/France_24_logo.svg/1200px-France_24_logo.svg.png" 
     }
 ];
 
-const player = videojs('my-video');
+const player = videojs('my-video', {
+    autoplay: false,
+    fluid: true,
+    html5: { vhs: { overrideNative: true }, nativeAudioTracks: false, nativeVideoTracks: false }
+});
+
 const list = document.getElementById('channelsList');
 
 channels.forEach(ch => {
-    const div = document.createElement('div');
-    div.className = 'channel-card';
-    div.innerHTML = `<img src="${ch.logo}"> <div>${ch.name}</div>`;
-    div.onclick = () => {
+    const card = document.createElement('div');
+    card.className = 'channel-card';
+    card.innerHTML = `<img src="${ch.logo}"><div>${ch.name}</div>`;
+    
+    card.onclick = () => {
         player.src({ type: 'application/x-mpegURL', src: ch.url });
         player.play();
-        document.getElementById('currentChannelName').innerText = "أنت تشاهد الآن: " + ch.name;
+        document.getElementById('currentChannelName').innerText = ch.name;
+        // صعود تلقائي للمشغل عند الضغط
+        window.scrollTo({top: 0, behavior: 'smooth'});
     };
-    list.appendChild(div);
+    list.appendChild(card);
 });
-
-// تشغيل القناة الأولى تلقائياً
-player.src({ type: 'application/x-mpegURL', src: channels[0].url });
